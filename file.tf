866413706e0191b57d5031b01a77ad9ae5d190b0
JonathanRosenboim <101969613+JonathanRosenboim@users.noreply.github.com>
2022-10-02 14:39:12 +0300

    Create file.tf

---
+++ file.tf
@@ -0,0 +1,7 @@
+resource "aws_s3_bucket_public_access_block" "positive2354354344543453543345534345453534354" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
+}
