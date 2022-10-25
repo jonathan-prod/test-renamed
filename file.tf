5479a7922b01f9d6fd31c7c86b504fb4c5b6cd35
JonathanRosenboim <101969613+JonathanRosenboim@users.noreply.github.com>
2022-10-25 11:06:36 +0300

    Update file.tf

--- file.tf
+++ file.tf
@@ -13,3 +13,12 @@ resource "aws_s3_bucket_public_access_block" "positive2354354344rerererer5434535
   block_public_policy = false
   ignore_public_acls  = false
 }
+
+
+resource "aws_s3_bucket_public_access_block" "podfesitive2354354344rerererer543453543345534345453534354" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
+}
