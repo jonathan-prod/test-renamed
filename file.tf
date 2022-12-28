173fe839d3f201c38eb32eb3e4b198451df74613
JonathanRosenboim <101969613+JonathanRosenboim@users.noreply.github.com>
2022-12-28 17:50:02 +0200

    Update file.tf

--- file.tf
+++ file.tf
@@ -13,3 +13,19 @@ resource "aws_s3_bucket_public_access_block" "positive2354354344rerererer5434535
   block_public_policy = false
   ignore_public_acls  = false
 }
+
+resource "aws_s3_bucket_public_access_block" "positive235435werwerwer344543453543345534345453534354" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
+}
+
+resource "aws_s3_bucket_public_access_block" "positive2354rewrwerewr354344rerererer543453543345534345453534354" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
+}
